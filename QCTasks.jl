14dc1aa6db528f3a364bfa0f473dd23cee6ab7dc
Yingjin Ma <yingjin_ma@163.com>
2022-03-04 17:09:11 +0800

    Done for generating NWChem tasks

--- QCTasks.jl
+++ QCTasks.jl
@@ -3,13 +3,22 @@ function nwchemtask(frag,atoms,par)
     # Generating NWChem headers
    
     fragidx=lpad(frag.idx,8,"0")
+    #run(`cp Template.nwchem $(fragidx).nw`) 
+ 
+    # 
+    open("Frag-$(fragidx).nw","w") do nwinp
+        txt = read("Template.nwchem",String)
+        txt = replace(txt,"FRAG-i" => "Frag-$(fragidx)","X-library-basis" => "* library 6-31g","dftxc" => "xc m06-2x")
+        print(nwinp,txt)
+    end     
 
-    #println(frag) 
-    run(`cp Template.nwchem $(fragidx).nw`) 
-
-    for iatom in atoms
-        #println(iatom)
+    open("XYZ.tmp","w") do xyztmp
+        for iatom in atoms
+            println(xyztmp,iatom.elem," ",iatom.coord[1]," ",iatom.coord[2]," ",iatom.coord[3])
+        end
     end 
+
+    run(`sed -i "4r XYZ.tmp" Frag-$(fragidx).nw`)
 
 end 
 
